5975ede04f64abd01381498b3c53f77c480f78a3
dxxlsxxm <dev.primesec@gmail.com>
2021-11-29 17:30:35 +0900

    [1215009-구자은] re-initialize directory

--- TDLApp/App.js
+++ TDLApp/App.js
@@ -7,17 +7,18 @@
  */
 
 import React from 'react'
-// import { createAppContainer } from 'react-navigation'
-// import { createStackNavigator } from 'react-navigation-stack'
-// import SplashScreen from './screens/SplashScreen'
-// import DetailScreen from './screens/DetailScreen'
-// import SettingScreen from './screens/SettingScreen'
-import { SafeAreaView, StyleSheet, Text } from 'react-native'
+import { SafeAreaView, StyleSheet, Text, View, TextInput, ScrollView } from 'react-native'
 
 const App = () => {
   return (
     <SafeAreaView style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.appTitle}>Hello To Do List</Text>
+      <View style={styles.card}>
+        <TextInput style={styles.input} placeholder='Add an item!' />
+        <ScrollView>
+          <Text>TodoList</Text>
+        </ScrollView>
+      </View>
     </SafeAreaView>
   )
 }
@@ -25,32 +26,32 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#3142e8'
+  },
+  appTitle: {
+    color: '#fff',
+    fontSize: 36,
+    marginTop: 30,
+    marginBottom: 30,
+    fontWeight: '300',
+    textAlign: 'center',
+    backgroundColor: '#3143e8'
+  },
+  card: {
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
+    flex: 1,
+    borderTopLeftRadius: 10,
+    borderTopRightRadius: 10,
+    marginLeft: 10,
+    marginRight: 10
+  },
+  input: {
+    padding: 20,
+    borderBottomColor: '#bbb',
+    borderBottomWidth: 1,
+    fontSize: 24,
+    marginLeft: 20
   }
 })
 
 export default App
-
-// const AppNavigator = createStackNavigator({
-//   Splash: SplashScreen,
-//   Detail: DetailScreen,
-//   Setting: SettingScreen
-// }, {
-//   defaultNavigationOptions: {
-//     headerStyle: {
-//       backgroundColor: '#000000'
-//     },
-//     headerTintColor: '#FFF'
-//   }
-// })
-// const Navigator = createAppContainer(AppNavigator)
-
-// export default function App () {
-//   return (
-//     <Navigator>
-//       <SplashScreen />
-//     </Navigator>
-//   )
-// }
